d37f178c05000c3dc5bed6a8bc678dbbee686b81
kvark <kvark@mozilla.com>
2017-02-27 23:21:40 -0800

    Basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+/*!
+Ticketed lock.
+
+Ticketed lock is similar to RwLock, except that the acquisition of the lock is split:
+
+  1. obtaining a ticket, which has to be done on the same thread as the locked storage
+  2. waiting on a ticket, which puts the current thread to sleep until the ticket is due. That moment comes when all the previous tickets are processed.
+  3. working with the data behind a read/lock guard
+  4. when the guard is freed, it allows the following tickets to become active
+
+A ticket can be moved between threads or even just lost.
+Consecutive read-only tickets do not guarantee a particular lock order.
+All the ticket counting is done based on `Arc` primitives, and the only unsafe code that this library has is for accessing the actual data behind a guard.
+
+*/
+#![warn(missing_docs)]
+
 mod raw;
 
 use std::{mem, ops};
@@ -5,6 +22,7 @@ use std::cell::UnsafeCell;
 use std::sync::Arc;
 
 
+/// The read-only guard of data, allowing `&T` dereferences.
 pub struct ReadLockGuard<T> {
     _inner: raw::LockGuard,
     data: Arc<UnsafeCell<T>>,
@@ -17,6 +35,7 @@ impl<T> ops::Deref for ReadLockGuard<T> {
     }
 }
 
+/// A ticket to read the data at some point.
 #[derive(Clone)]
 pub struct ReadTicket<T> {
     inner: raw::Ticket<raw::Read>,
@@ -26,6 +45,7 @@ pub struct ReadTicket<T> {
 unsafe impl<T> Send for ReadTicket<T> {}
 
 impl<T> ReadTicket<T> {
+    /// Wait for the ticket to become active, returning a lock guard.
     pub fn wait(self) -> ReadLockGuard<T> {
         ReadLockGuard {
             _inner: self.inner.wait(),
@@ -35,6 +55,7 @@ impl<T> ReadTicket<T> {
 }
 
 
+/// The read-only guard of data, allowing `&T` dereferences.
 pub struct WriteLockGuard<T> {
     _inner: raw::LockGuard,
     data: Arc<UnsafeCell<T>>,
@@ -53,6 +74,7 @@ impl<T> ops::DerefMut for WriteLockGuard<T> {
     }
 }
 
+/// A ticket to read/write the data at some point.
 pub struct WriteTicket<T> {
     inner: raw::Ticket<raw::Write>,
     data: Arc<UnsafeCell<T>>,
@@ -61,6 +83,7 @@ pub struct WriteTicket<T> {
 unsafe impl<T> Send for WriteTicket<T> {}
 
 impl<T> WriteTicket<T> {
+    /// Wait for the ticket to become active, returning a lock guard.
     pub fn wait(self) -> WriteLockGuard<T> {
         WriteLockGuard {
             _inner: self.inner.wait(),
@@ -70,12 +93,14 @@ impl<T> WriteTicket<T> {
 }
 
 
+/// The ticketed lock, which wraps the data.
 pub struct TicketedLock<T> {
     inner: raw::TicketedLock,
     data: Arc<UnsafeCell<T>>,
 }
 
 impl<T> TicketedLock<T> {
+    /// Create a new ticketed lock.
     pub fn new(data: T) -> TicketedLock<T> {
         TicketedLock {
             inner: raw::TicketedLock::new(),
@@ -83,6 +108,7 @@ impl<T> TicketedLock<T> {
         }
     }
 
+    /// Remove the lock and extract the data out.
     pub fn unlock(mut self) -> T {
         self.inner.flush();
         match Arc::try_unwrap(self.data) {
@@ -91,6 +117,7 @@ impl<T> TicketedLock<T> {
         }
     }
 
+    /// Acquire a read-only ticket.
     pub fn read(&mut self) -> ReadTicket<T> {
         ReadTicket {
             inner: self.inner.read(),
@@ -98,6 +125,7 @@ impl<T> TicketedLock<T> {
         }
     }
 
+    /// Acquire a write-only ticket.
     pub fn write(&mut self) -> WriteTicket<T> {
         WriteTicket {
             inner: self.inner.write(),
